ae428bb331da6406b729f2f57a9c02c639c27b69
arthurmarks349 <arthurmarks349@gmail.com>
2015-02-21 14:49:21 -0500

    modal login
    modal login created

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
 
-$pageContents = <<< EOPAGE
 <!doctype html>
 <html lang="en">
 <head>
@@ -12,7 +10,8 @@ $pageContents = <<< EOPAGE
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 	<link rel="stylesheet" href="css/main-custom.css">
-
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </head>
 
@@ -26,7 +25,7 @@ $pageContents = <<< EOPAGE
 				<li class="li_Nav"><a href="#">About</a></li>
 				<li class="li_Nav"><a href="#">Contact</a></li>
 				<li class="li_Nav"><a href="#">Browse</a></li>
-				<li class="li_Nav"><a href="#">Login</a></li>
+				<li class="li_Nav" data-toggle="modal" data-target="#myModal"><a href="#">Login</a></li>
 			</ul>
 		</div>
 	</div>
@@ -39,10 +38,40 @@ $pageContents = <<< EOPAGE
 			<p class="text-muted">Product of Steel City Codefest 2015.</p>
 		</div>
 	</footer>
+	
+	<!-- Modal -->
+	<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="myModalLabel">Please Log in or Register Below...</h4>
+				</div>
+				<div class="modal-body">
+					<div id="div_login">
+						<form class="login" id="login_form" autocomplete="off"> 
+							<p>
+								<label for="login">Username:</label>
+								<input type="text" name="login" id="login" value="">  
+							</p>
+
+							<p>
+								<label for="password">Password:</label>
+								<input type="password" name="password" id="password" value="">
+							</p>
+						</form>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" id="register" class="btn btn-primary">Register</button>
+					<button type="button" id="login" class="btn btn-primary">Login</button>
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					
+				</div>
+			</div>
+		</div>
+	</div>
 
 </body>
 </html>
-EOPAGE;
 
-echo $pageContents;
-?>
