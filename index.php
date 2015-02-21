937ad14af89e777a7ea55bdb6610a92f14283c51
Phillip Green II <phillip.green.ii@gmail.com>
2015-02-21 12:46:53 -0500

    added jquery

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-   
-   
+
 $pageContents = <<< EOPAGE
 <!doctype html>
 <html lang="en">
@@ -13,6 +12,7 @@ $pageContents = <<< EOPAGE
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
 
+    <script src="js/jquery-2.1.3.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 </head>
 
