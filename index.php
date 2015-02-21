5d219281f9ca4b425726022d1105cc8f885ff0c9
Phillip Green II <phillip.green.ii@gmail.com>
2015-02-21 12:38:36 -0500

    added bootstrap

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ $pageContents = <<< EOPAGE
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Coder's Haven</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <script src="js/bootstrap.min.js"></script>
 </head>
 
 <body>
