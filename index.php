6dee65dc0491f48cba8555e580f916d737e335e9
Phillip Green II <phillip.green.ii@gmail.com>
2015-02-21 12:27:12 -0500

    initial page

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+$pageContents = <<< EOPAGE
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Coder's Haven</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+	<div id="head">
+    <h1>Coder's Haven</h1>
+	</div>
+
+  <div id="content">
+  </div>
+	<ul id="foot">
+	</ul>
+</body>
+</html>
+EOPAGE;
+
+echo $pageContents;
+?>
