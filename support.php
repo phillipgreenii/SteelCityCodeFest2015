4f9d0a5faa5df8a3792d927a9be6d472603dd600
Phillip Green II <phillip.green.ii@gmail.com>
2015-02-22 00:03:26 -0500

    usage path on redirect

--- support.php
+++ support.php
@@ -3,6 +3,6 @@ function add_location_header($path="") {
   /* Redirect to a different page in the current directory that was requested */
   $host  = $_SERVER['HTTP_HOST'];
   $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
-  header("Location: http://$host$uri/");
+  header("Location: http://$host$uri/$path");
 }
 ?>
