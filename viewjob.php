e40eceaa9c134f128646dddb0048f2215aecdef6
seantaylor32 <seantaylor32@gmail.com>
2015-02-21 20:31:17 -0500

    View/Create Job - Version 1
    Necessary fields to view and create jobs

---
+++ viewjob.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<label for="textfield">Job Title:</label>
+<input type="text" name="jobtitle" id="jobtitle"><br>
+<label for="textfield">Description:</label>
+<input type="text" name="description" id="description"><br>
+<label for="textfield">Requirements:</label>
+<input type="text" name="requirements" id="requirements"><br>
+<label for="textfield">Company Info:</label>
+<input type="text" name="companyinfo" id="companyinfo"><br>
+<label for="textfield">Pay:</label>
+<input type="text" name="pay" id="pay"><br>
+<label for="textfield">Duration:</label>
+<input type="text" name="duration" id="duration"><br>
+<label for="textfield">Benefits:</label>
+<input type="text" name="benefits" id="benefits"><br>
+</body>
+</html>
