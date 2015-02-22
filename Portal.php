efa4e97d459460ba68b37876fdfa90121312eb41
Phillip Green II <phillip.green.ii@gmail.com>
2015-02-22 00:28:25 -0500

    added user_is_candidate() and user_is_employer()

--- Portal.php
+++ Portal.php
@@ -3,9 +3,32 @@ include_once 'template.php';
 include_once 'user.php';
 user_redirect_if_not_authenticated();
 
+$links = '';
+
+if(user_is_candidate()) {
+  $links = $links . <<<EOL
+  <div>
+  <h2>Candidate Links</h2>
+  <ul>
+  <li><a href="ViewResume.php">View Resume</a></li>
+  </ul>
+  </div>
+EOL;
+}
+
+if(user_is_employer()) {
+  $links = $links . <<<EOL
+  <div>
+  <h2>Employeer Links</h2>
+  <ul>
+  <li><a href="ViewJobs.php">View Jobs</a></li>
+  </ul>
+  </div>
+EOL;
+}
 
 $pageContents = <<< EOPAGE
-
+{$links}
 EOPAGE;
 
 echo wrap_full_template($pageContents);
--- user.php
+++ user.php
@@ -15,6 +15,14 @@ function user_is_authenticated() {
   return isset($_SESSION['user_id']);
 }
 
+function user_is_candidate() {
+  return in_array('Candidate', $_SESSION['user_roles']);
+}
+
+function user_is_employer() {
+  return in_array('Employer', $_SESSION['user_roles']);
+}
+
 function user_logout() {
   session_start();
 
@@ -159,9 +167,28 @@ function user_authenticate($user_name, $password) {
 
   if($authenticated) {
     $_SESSION['user_id'] = $credentials->user_id;
+    $_SESSION['user_roles'] = _load_user_roles($credentials->user_id);
   }
 
   return $authenticated;
+}
+
+function _load_user_roles($user_id) {
+  $connection = db_open();
+
+  //find role ids
+  $roles = [];
+  $query = 'select RoleName from roles r join userroles ur on ur.RoleId = r.RoleId where ur.UserId = ?';
+  $statement = $connection->prepare($query);
+  $statement->bind_param('i', $user_id);
+  $statement->bind_result($role);
+  $statement->execute();
+  while ($statement->fetch()) {
+    $roles[] = $role;
+  }
+  $statement->close();
+
+  return $roles;
 }
 
 function _user_generate_salt() {
